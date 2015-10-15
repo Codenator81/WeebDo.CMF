3420b40ec39d281f248f069976c57e39d6f4b4a3
Codenator81 <codenator81@gmail.com>
2015-10-16 00:39:10 +0300

    Little cleans in ProtectFolderMiddleware

--- src/WeebDoCMF/Areas/WDCore/Middleware/ProtectFolderMiddleware.cs
+++ src/WeebDoCMF/Areas/WDCore/Middleware/ProtectFolderMiddleware.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Identity;
 using Microsoft.Extensions.Logging;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using WeebDoCMF.WDCore.Models;
 
 namespace WeebDoCMF.WDCore.Middleware
 {
@@ -14,25 +11,19 @@ namespace WeebDoCMF.WDCore.Middleware
         private readonly RequestDelegate _next;
         private readonly PathString _path;
         private readonly string _roleName;
-        public UserManager<WeebDoCmfUser> _userManager { get; private set; }
         private readonly ILogger _logger;
 
-        public ProtectFolderMiddleware(UserManager<WeebDoCmfUser> userManager, RequestDelegate next, ProtectFolderOptions options, ILoggerFactory loggerFactory)
+        public ProtectFolderMiddleware(RequestDelegate next, ProtectFolderOptions options, ILoggerFactory loggerFactory)
         {
             _next = next;
             _path = options.Path;
             _roleName = options.RoleName;
-            _userManager = userManager;
             _logger = loggerFactory.CreateLogger(typeof(ProtectFolderMiddleware).FullName);
         }
 
         public async Task Invoke(HttpContext httpContext,
                                  IAuthorizationService authorizationService)
-        {
-            //_logger.LogWarning("Starting");
-            //var useridaa = httpContext.User.Identity.IsAuthenticated;
-            //_logger.LogWarning("httpContext.User:useridaa" + useridaa);
-            
+        {            
             if (httpContext.Request.Path.StartsWithSegments(_path))
             {
                 var authorized = httpContext.User.IsInRole(_roleName);
